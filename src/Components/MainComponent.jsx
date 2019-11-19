7e08624fa3efc70b1b2ecab175c11aba32b446e3
henlovelybird <voxylophone@gmail.com>
2019-11-19 12:28:56 +0100

    Setting up components

---
+++ src/Components/MainComponent.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Container, Row, Col} from "reactstrap";
+import ClaimComponent from './Components/ClaimComponent';
+
+
+class MainComponent extends React.Component {
+    state ={ }
+    render() {
+        return (
+            <Container>
+               <Row>
+                   <Col md="12">
+                       
+                   </Col>
+               </Row>
+            </Container>
+        );
+    }
+}
+
+export default MainComponent;
