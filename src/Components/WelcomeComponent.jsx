fdccea1b0683da684280b7e8cf89cd38802c761f
henlovelybird <voxylophone@gmail.com>
2019-11-20 11:28:34 +0100

    more css

--- src/Components/WelcomeComponent.jsx
+++ src/Components/WelcomeComponent.jsx
@@ -8,7 +8,7 @@ class WelcomeComponent extends React.Component {
             <Jumbotron fluid className="bg">
               <Container fluid>
                 <h1 className="display-3">Welcome to My Bookstore!</h1>
-                <p className="lead">I actually finished an assignment after a month of pure failure!</p>
+                <p className="lead">Let' get some books!</p>
               </Container>
             </Jumbotron>
         );
